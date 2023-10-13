2ff9fcff3a3877e18ff36e1dbf9f26f9433406cf
Amin <147066635+amin13m@users.noreply.github.com>
2023-10-12 23:31:24 -0700

    forground added

--- index.js
+++ index.js
@@ -12,21 +12,33 @@ let bg={
     sy:0,
     w:150,
     h:226,
-    x:-5,
-    y:ca.height-226,
+    x:0,
+    y:ca.height-236,
     draw(){
         ctx.drawImage(
             sprite,this.sx,this.sy,this.w,this.h,
-            this.x,this.y,this.w*2,this.h)
-            
-            ctx.drawImage(
-                sprite,this.sx,this.sy,this.w,this.h,
-                this.w,this.y,this.w*2,this.h)
+            this.x,this.y,this.w,this.h)
+
+        ctx.drawImage(
+            sprite,this.sx,this.sy,this.w,this.h,
+            this.w-6,this.y,this.w,this.h)
     }
 }
 
 
-
+let fg={
+    sx:220,
+    sy:-60,
+    w:170,
+    h:120,
+    x:-5,
+    y:ca.height-108,
+    draw(){
+        ctx.drawImage(
+            sprite,this.sx,this.sy,this.w,this.h,
+            this.x,this.y,this.w*2,this.h)
+    }
+}
 
 
 
@@ -45,6 +57,7 @@ function draw(){
     ctx.fillStyle="#70c5ce"
     ctx.fillRect(0,0,ca.width,ca.height)
     bg.draw()
+    fg.draw()
 }
 
 function animate(){
