9593145554f7e614f4f0ec3538b019ce6bbf0b01
Amin <147066635+amin13m@users.noreply.github.com>
2023-10-12 23:00:56 -0700

    background add

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ let fram=0
 
 let sprite = new Image()
 
-sprite.src="image/template.jpg"
+sprite.src="image/template.png"
+
+let bg={
+    sx:0,
+    sy:0,
+    w:150,
+    h:226,
+    x:-5,
+    y:ca.height-226,
+    draw(){
+        ctx.drawImage(
+            sprite,this.sx,this.sy,this.w,this.h,
+            this.x,this.y,this.w*2,this.h)
+            
+            ctx.drawImage(
+                sprite,this.sx,this.sy,this.w,this.h,
+                this.w,this.y,this.w*2,this.h)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 function update(){
 
@@ -14,6 +44,7 @@ function update(){
 function draw(){
     ctx.fillStyle="#70c5ce"
     ctx.fillRect(0,0,ca.width,ca.height)
+    bg.draw()
 }
 
 function animate(){
